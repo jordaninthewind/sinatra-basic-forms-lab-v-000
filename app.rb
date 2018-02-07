4ac210a43ec616c8dccd4df671a60378534f221a
Jordan Kline <jordan.kline@gmail.com>
2018-02-07 19:44:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,12 +8,13 @@ class App < Sinatra::Base
   end
 
   post '/' do
-
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    
     erb :display_puppy
   end
 
   get '/new' do
-    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+
 
     erb :create_puppy
   end
